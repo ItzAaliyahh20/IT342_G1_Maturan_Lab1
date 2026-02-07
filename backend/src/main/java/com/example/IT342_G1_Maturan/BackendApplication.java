fad26ba968c5a5b3eccf68fe99bc5b4752f0e370
Frances Aaliyah Maturan <maturanfrances123@gmail.com>
2026-02-07 09:14:30 +0800

    add the springboot folder

---
+++ backend/src/main/java/com/example/IT342_G1_Maturan/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.IT342_G1_Maturan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/IT342_G1_Maturan/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IT342_G1_Maturan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
