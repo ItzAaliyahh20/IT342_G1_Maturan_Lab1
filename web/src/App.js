6c2bd92f5e020a3cc63a8096c71199434763b169
LAB13_USER ON WS19 <L13Y09W19@LabHCI.net>
2026-02-14 10:35:16 +0800

    added the unfinish checklist

--- web/src/App.js
+++ web/src/App.js
@@ -21,6 +21,7 @@ function App() {
               </PrivateRoute>
             }
           />
+          
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="*" element={<Navigate to="/dashboard" replace />} />
         </Routes>
